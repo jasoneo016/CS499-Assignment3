124a1bf13b0b16f2ee5f22e40ad99cd82fe13c76
Jason Zhang <jasoneo016@gmail.com>
2017-02-28 18:35:14 -0800

    first commit

---
+++ src/main/java/cpp/edu/cs499/MapReduce/App.java
@@ -0,0 +1,13 @@
+package cpp.edu.cs499.MapReduce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
