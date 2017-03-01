491a92d5a1c172a97b49074cc3d89af01b81a1ca
Jason Zhang <jasoneo016@gmail.com>
2017-02-28 20:12:37 -0800

    psuedocode in MapClass for basic logic of top 10 movies average rating

--- src/main/java/cpp/edu/cs499/MapReduce/MapClass.java
+++ src/main/java/cpp/edu/cs499/MapReduce/MapClass.java
@@ -12,18 +12,34 @@ public class MapClass extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 	private static final IntWritable one = new IntWritable(1);
 	private Text word = new Text();
+	
+	private String temp;
 
+	//Top 10 Movies that have the highest average ratings
 	@Override
 	protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
 			throws IOException, InterruptedException {
 
-		String line = value.toString();
-		StringTokenizer st = new StringTokenizer(line, " ");
-
-		while(st.hasMoreTokens()) {
-			word.set(st.nextToken());
-			context.write(word, one);
-		}
+		String first = value.toString().split(",")[0];
+		String last = value.toString().split(",")[2];
+		
+		System.out.println(first);
+		
+		/*
+		 * int count = 0;
+		 * while temp = first {
+		 * count++;
+		 * double ratings = 0;
+		 * double ratings += last.toInteger();
+		 * 
+		 * if (temp != first) {
+		 * temp = first;
+		 * 
+		 * 
+		 * ratings / count;
+		 */
+		
+		word.set(first);
+		context.write(word, one);
 	}
-
 }
